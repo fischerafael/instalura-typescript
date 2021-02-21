e67705e5f9ee51e7ff7c9ed1954db258c161796a
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-02-20 21:06:32 -0300

    03: Added font and finished menu component

--- src/components/commons/Menu/index.tsx
+++ src/components/commons/Menu/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
+
 import Logo from '../Logo'
 import { MenuWrapper } from './styles'
+
+import { menuLinks } from '../../../utils/menu-links'
 
 const Menu = () => {
     return (
@@ -8,7 +11,17 @@ const Menu = () => {
             <MenuWrapper.Left>
                 <Logo />
             </MenuWrapper.Left>
-            <MenuWrapper.Center></MenuWrapper.Center>
+            <MenuWrapper.Center as="ul">
+                {menuLinks.map((link) => (
+                    <li key={link.url}>
+                        <a href={link.url}>{link.name}</a>
+                    </li>
+                ))}
+            </MenuWrapper.Center>
+            <MenuWrapper.Left>
+                <button>Entrar</button>
+                <button>Cadastrar</button>
+            </MenuWrapper.Left>
         </MenuWrapper>
     )
 }
